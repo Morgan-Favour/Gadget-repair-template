ddee10722e4394d094994f7b6034e9b81595e9c2
Morgan Favour <morganfavour85@gmail.com>
2025-07-07 11:24:24 +0100

    Refactor code structure for improved readability and maintainability

--- src/main.js
+++ src/main.js
@@ -61,3 +61,19 @@ document.querySelectorAll('.toggle-btn').forEach((btn) => {
     }
   });
 });
+
+function openModal() {
+    document.getElementById('contactModal').classList.remove('hidden');
+  }
+
+  function closeModal() {
+    document.getElementById('contactModal').classList.add('hidden');
+  }
+
+  // Optional: close modal if you click outside the modal box
+  window.addEventListener('click', function (e) {
+    const modal = document.getElementById('contactModal');
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
